f20bf0b06c87b103664b09bc8b50099281cd549e
Helder Nicollas <nicollashelder@gmail.com>
2023-04-02 00:25:32 -0300

    update title alteration

--- src/components/notes/list/index.jsx
+++ src/components/notes/list/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Button, Column, Tag, Title, List } from "rbx";
+import React, { useState } from 'react';
+import { Button, Column, Tag, Title, List, Input } from "rbx";
 import Moment from 'moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { AiFillEdit } from 'react-icons/ai';
 import '../../../styles/notes.scss';
 
 
 function ListNotes(props) {
+    const [isNoteTitleAlteration, setIsNoteTitleAlteration] = useState(false);
+    const [noteTitle, setNoteTitle] = useState('');
+
     return (
         <>
             <Column.Group breakpoint="mobile">
@@ -25,14 +29,43 @@ function ListNotes(props) {
             <List className="notes-list">
                 {props.notes.map((item, key) =>
                     <List.Item key={key} onClick={() => props.selectNote(item._id)} active={item == props.currentNote}>
-                        <Title size={6}>
-                            {/* Remover todas as tags HTML e apresentar somente o texto */}
-                            {/* {item.body.replace(/(<([^>]+)>)/ig, "").substring(0, 25)} */}
-                            {
+                        {/* {item.body.replace(/(<([^>]+)>)/ig, "").substring(0, 25)} */}
+                        {/* {
                                 ['<h1>', '<h2>', '<b>'].includes(item.body.split(/(<([^>]+)>)/ig)[1]) ?
                                     item.body.split(/(<([^>]+)>)/ig)[3] :  item.body.replace(/(<([^>]+)>)/ig, "").substring(0, 25)
-                            }
+                            } */}
+
+                        <Title size={5} >
+                            <Column.Group className="is-vcentered" id="note-title-column">
+                                <form action="" method="">
+                                    <Column size={12}>
+                                        {
+                                            isNoteTitleAlteration ?
+                                                <Input
+                                                    type="text"
+                                                    name="title"
+                                                    required
+                                                    value={noteTitle}
+                                                    onChange={event => setNoteTitle(event.target.value)}
+                                                    onBlur={() => setIsNoteTitleAlteration(!isNoteTitleAlteration)}
+                                                    id="note-title"
+                                                    autoFocus
+                                                /> : item.title
+                                        }
+                                    </Column>
+                                </form>
+                                <Column size={2}>
+                                    {
+                                        !isNoteTitleAlteration &&
+                                        <AiFillEdit id="edit-icon" onClick={() => {
+                                            setIsNoteTitleAlteration(!isNoteTitleAlteration);
+                                            setNoteTitle(item.title);
+                                        }} />
+                                    }
+                                </Column>
+                            </Column.Group>
                         </Title>
+
                         <Title size={6} subtitle spaced={false}>
                             {
                                 item.body.replace(/(<([^>]+)>)/ig, "").substring(0, 30) + '...'
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const Api = axios.create({baseURL: 'https://javascriptnotesapi.onrender.com'});
-const Api = axios.create({baseURL: 'https://javascriptnotesapi.onrender.com'});
+const Api = axios.create({baseURL: 'http://localhost:8000'});
 
 
 export default Api;
